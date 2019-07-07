21bc6cd3a2fa951a40e0d8798f5fe91231e7f31d
giles14 <giles.14@gmail.com>
2019-07-07 13:10:25 -0500

    initial commit
    Primera versión del código, hasta éste punto puede mostrar la tabla de CFDI emitidos (guardados en /emitidos).

---
+++ class-factura.php
@@ -0,0 +1,190 @@
+<?php
+//Solo para fines de desarrollo: quitar
+ini_set('display_errors', 1);
+
+$_ERRORS = array();
+
+function ErrorHandler($errno, $errstr, $errfile, $errline) {
+    global $_ERRORS;
+    $_ERRORS[] = array("errno" => $errno, "errstr" => $errstr, "errfile" => $errfile, "errline" => $errline);
+}
+
+set_error_handler("ErrorHandler");
+
+$dir = $_SERVER['DOCUMENT_ROOT'].'/fl/emitidos';
+$filesEmitidos = array_slice(scandir($dir), 2);
+$dirR = $_SERVER['DOCUMENT_ROOT'].'/fl/recibidos';
+$filesR = array_slice(scandir($dirR), 2);
+
+global $iva;
+global $total;
+
+$contador = 0;
+$subTotal = 0;
+$iva = 0;
+$total = 0;
+
+//$argsEmitidos = array("listFiles" => $filesEmitidos);
+
+
+function printEmitedTable($argsEmitidos){
+  global $filesEmitidos;
+  $listEmitidos = $filesEmitidos;
+
+  $iva = 0;
+  $subTotal = 0;
+  $total = 0;
+
+//$listEmitidos = $argsEmitidos["listFiles"];
+//$styleCss = $argsEmitidos["cssClasses"];
+$styleCss = $argsEmitidos;
+echo '<table id="tabla" class="'.$styleCss.'">';
+echo '<thead>';
+echo '	<tr>';
+echo '		<th>Fila</th>';
+echo '		<th>RFC</th>';
+echo '		<th class="hidden-1024">Receptor</th>';
+echo '		<th class="hidden-350">Subtotal</th>';
+echo '		<th class="hidden-480">IVA</th>';
+echo '		<th>Total</th>';
+echo '	</tr>';
+echo '</thead>';
+
+echo '<tbody>';
+foreach($listEmitidos as $file){
+		//print $file . '<br/>';
+		$factura = new factura;
+		$contador ++;
+
+		$factura->readXml('emitidos/'.$file,$contador);
+		$factura->printTr();
+		$subTotal += $factura->returnSubTotal();
+		$total += $factura->returnTotal();
+		$iva += $factura->returnIva();
+		}
+	echo '</tbody>';
+	echo '</table>';
+  echo '</div>';
+  echo '<div class="container-fluid">';
+  echo '<div class="totales">';
+  echo 'iva: '. $iva . '<br />';
+  echo 'subtotal: '. $subTotal . '<br />';
+  echo 'Total: ' . $total . '<br />';
+  echo '</div>';
+  echo '</div>';
+
+}
+
+class factura{
+	private $fileToRead;
+	private $xml;
+    public $emisorRfc;
+    public $emisorRazonSocial;
+    public $receptorRfc;
+    public $receptorRazonSocial;
+    public $subTotal;
+    public $total;
+    public $Iva;
+    public $contador;
+
+
+	public function readXml($fileToRead, $row)
+	{
+        $this->contador = $row;
+		$xml = simplexml_load_file($fileToRead);
+		$ns = $xml->getNamespaces(true);
+		$xml->registerXPathNamespace('c', $ns['cfdi']);
+		$xml->registerXPathNamespace('t', $ns['tfd']);
+
+        foreach ($xml->xpath('//cfdi:Comprobante') as $cfdiComprobante){
+            $this->subTotal = strval($cfdiComprobante['SubTotal']);
+            $this->total = strval($cfdiComprobante['Total']);
+
+        }
+
+
+		foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Emisor') as $Emisor){
+          $this->emisorRfc = strval($Emisor['Rfc']);
+          $this->emisorRazonSocial = strval($Emisor['Nombre']);
+
+		}
+        foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Impuestos//cfdi:Traslados//cfdi:Traslado') as $Traslado){
+           $this->Iva = strval($Traslado['Importe']);
+        }
+        foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Receptor') as $Receptor){
+           $this->receptorRfc = strval($Receptor['Rfc']);
+           $this->receptorRazonSocial = strval($Receptor['Nombre']);
+        }
+	}
+    public function printTr()
+    {
+
+        echo '<tr>';
+        echo '<td>' . $this->contador . '</td>';
+        echo '<td>' . $this->receptorRfc . '</td>';
+        echo '<td class="hidden-1024">' . $this->receptorRazonSocial . '</td>';
+        echo '<td class="hidden-350"> $ ' . number_format($this->subTotal,2) . '</td>';
+        echo '<td class="hidden-480"> $ ' . number_format($this->Iva,2) . '</td>';
+        echo '<td> $ ' . number_format($this->total,2) . '</td>';
+        echo '</tr>';
+
+    }
+    public function printTrEmisor()
+    {
+        echo '<tr>';
+        echo '<td>' . $this->contador . '</td>';
+        echo '<td>' . $this->emisorRfc . '</td>';
+        echo '<td  class="hidden-1024">' . $this->emisorRazonSocial . '</td>';
+        echo '<td class="hidden-350"> $ ' . number_format($this->subTotal,2) . '</td>';
+        echo '<td class="hidden-480"> $ ' . number_format($this->Iva,2) . '</td>';
+        echo '<td> $ ' . number_format($this->total,2) . '</td>';
+        echo '</tr>';
+
+    }
+    public function returnSubTotal(){
+        return $this->subTotal;
+    }
+
+    public function returnTotal(){
+        return $this->total;
+    }
+
+    public function returnIva(){
+        return $this->Iva;
+    }
+
+}
+ function tableDiot($rfcs){
+    echo '<table class="azul">';
+    echo '<tr>';
+    echo '<th>';
+    echo 'RFC';
+    echo '</th>';
+    echo '<th>';
+    echo 'Cantidad';
+    echo '</th>';
+    echo '</tr>';
+
+    foreach($rfcs as $key => $value) {
+    echo '<tr>';
+        echo '<td>';
+            echo "$key";
+        echo '</td>';
+        echo '<td>';
+            echo "$value";
+        echo '</td>';
+    echo '</tr>';
+    }
+    echo '<tfoot>';
+    echo '<tr>';
+        echo '<td> Total';
+        echo '</td>';
+        echo '<td>';
+            print_r(array_sum($rfcs));
+        echo '</td>';
+    echo '<tr>';
+    echo '</tfoot>';
+
+    echo '</table>';
+ }
+ ?>
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php include "class-factura.php" ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Bootstrap -->
+
+    <link rel="stylesheet" href="css/plugins/datatable/TableTools.css">
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
+
+
+
+    <title>Visor Automático de CFDI</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg  navbar-custom">
+      <a class="navbar-brand" href="#">Visor automático de CFDI's</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Visor <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Subir CFDI's</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Categorías</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Opciones
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Privacidad</a>
+              <a class="dropdown-item" href="#">Cuenta</a>
+              <a class="dropdown-item" href="#">Otra opción</a>
+            </div>
+          </li>
+        </ul>
+        <span class="ml-auto navbar-text">Ver 0.1</span>
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <h1>Tablas</h1>
+      <?php
+      //$argsEmitidos["cssClasses"] = "table table-bordered table-hover table-nomargin table-striped dataTable dataTable-colreorder";
+      printEmitedTable("table table-bordered table-hover table-nomargin table-striped dataTable dataTable-colreorder");
+      ?>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <script type="text/javascript">
+    $(document).ready( function () {
+    $('#tabla').DataTable({
+      "language": {
+            "lengthMenu": "Mostrando _MENU_ filas por página",
+            "zeroRecords": "No hay datos para mostrar - disculpa",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "Sin datos",
+            "infoFiltered": "(Filtrado de _MAX_ total de registros)"
+      }
+    });
+    } );
+    </script>
+
+  </body>
+</html>
